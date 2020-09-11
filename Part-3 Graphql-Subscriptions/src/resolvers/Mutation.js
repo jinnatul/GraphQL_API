2e68201d5e5d9929128d6e698ef2f6ae354c3f0d
jinnatul <morolswediu@gmail.com>
2020-09-11 21:27:45 +0600

    subscription for post (create & update & delete)

--- Part-3 Graphql-Subscriptions/src/resolvers/Mutation.js
+++ Part-3 Graphql-Subscriptions/src/resolvers/Mutation.js
@@ -86,29 +86,43 @@ const Mutation = {
     db.posts.push(post);
 
     if (args.data.published) {
-      pubsub.publish('post', { post })
+      pubsub.publish('post', { 
+        post: {
+          mutation: 'CREATED',
+          data: post
+        }
+      })
     }
 
     return post;
   },
-  deletePost(parent, args, { db }, info) {
+  deletePost(parent, args, { db, pubsub}, info) {
     const postIndex = db.posts.findIndex(post => post.id === args.id);
 
     if (postIndex === -1) {
       throw new Error("Post not found.")
     }
 
-    const deletePosts = db.posts.splice(postIndex, 1);
+    const [post] = db.posts.splice(postIndex, 1);
 
     db.comments = db.comments.filter((comment) => {
       comment.post !== args.id;
     });
 
-    return deletePosts[0];
+    if (post.published) {
+      pubsub.publish('post', { 
+        post: {
+          mutation: 'DELETED',
+          data: post
+        }
+      })
+    }
+    return post;
   },
-  updatePost(parent, args, { db }, info) {
+  updatePost(parent, args, { db, pubsub}, info) {
     const { id, data } = args;
     const post = db.posts.find((post) => post.id === id);
+    const originalPost = { ...post };
 
     if (!post) {
       throw new Error("Post not found");
@@ -124,6 +138,28 @@ const Mutation = {
 
     if (typeof data.published === "boolean") {
       post.published = data.published;
+      if (originalPost.published && !post.published) {
+        pubsub.publish('post', {
+          post: {
+            mutation: 'DELETED',
+            data: originalPost
+          }
+        })
+      } else if (!originalPost.published && post.published) {
+        pubsub.publish('post', {
+          post: {
+            mutation: 'CREATED',
+            data: post
+          }
+        })
+      }
+    } else if (post.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'UPDATED',
+          data: post
+        }
+      })
     }
 
     return post;
@@ -131,7 +167,7 @@ const Mutation = {
   createComment(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some((user) => user.id === args.data.author);
     const postExists = db.posts.some(
-      (post) => post.id === args.data.post && post.publish
+      (post) => post.id === args.data.post && post.published
     );
 
     if (!userExists || !postExists) {
